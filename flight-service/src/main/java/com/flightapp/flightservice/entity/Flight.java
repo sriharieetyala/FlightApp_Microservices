0dbcd73992c0b2b9a34c00cdcffd86b6a45fa5ea
sriharieetyala <sriharieetyala143@gmail.com>
2025-11-30 12:15:53 +0530

    added entity models and DTO structures for flight creation, retrieval

---
+++ flight-service/src/main/java/com/flightapp/flightservice/entity/Flight.java
@@ -0,0 +1,41 @@
+package com.flightapp.flightservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "flights", uniqueConstraints = @UniqueConstraint(columnNames = "flightNumber"))
+public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+
+    @Column(nullable = false)
+    private String flightNumber;
+
+    @Column(nullable = false)
+    private String fromCity;
+
+    @Column(nullable = false)
+    private String toCity;
+
+    @Column(nullable = false)
+    private LocalDateTime departureTime;
+
+    @Column(nullable = false)
+    private LocalDateTime arrivalTime;
+
+    @Column(nullable = false)
+    private float cost;
+
+    @Column(nullable = false)
+    private Integer seatsAvailable;
+}
+
